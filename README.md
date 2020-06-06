c00e7b5fddd1b1b3475e815d820c5adf88694b45
Laragrad Dev <dev@laragrad.com>
2020-06-06 10:20:30 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# laragrad/eloquent-model-pg-array-relations
+
+This package provides a trait `\Laragrad\Models\Concerns\HasPgArrayRelations` that adds into `Eloquent\Model` some PostgreSQL specific fields cast types.
+
+## Installing
+
+Run command in console
+
+	composer require laragrad/eloquent-model-pg-array-relations
+
---
+++ src/Relations/PgArrayRelation.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Laragrad\Relations;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Relations\Relation;
+
+abstract class PgArrayRelation extends Relation
+{
+
+    /**
+     * [Description] TODO
+     *
+     * @var string
+     */
+    protected $arrayField;
+
+    /**
+     * [Description] TODO
+     *
+     * @var string
+     */
+    protected $relatedKey;
+
+    protected $castAs;
+
+    /**
+     * Constructor
+     *
+     * @param Builder $query
+     * @param Model $related
+     * @param null|string $arrayField
+     * @param null|string $relatedKey
+     * @param null|string $castAs
+     */
+    public function __construct(Builder $query, Model $parent, $arrayField, $relatedKey, $castAs = null)
+    {
+        $this->arrayField = $arrayField;
+        $this->relatedKey = $relatedKey;
+        $this->castAs = $castAs;
+
+        parent::__construct($query, $parent);
+    }
+
+    /**
+     * Initialize the relation on a set of models.
+     *
+     * @param  array   $models
+     * @param  string  $relation
+     * @return array
+     */
+    public function initRelation(array $models, $relation)
+    {
+
+        foreach ($models as $model) {
+            $model->setRelation($relation, $this->related->newCollection());
+        }
+
+        return $models;
+    }
+
+    /**
+     * Get the results of the relationship.
+     *
+     * @return mixed
+     */
+    public function getResults()
+    {
+        return $this->get();
+    }
+
+    protected function getCastAs($isArray = false)
+    {
+        if (is_null($this->castAs)) {
+            return "";
+        }
+
+        return "::{$this->castAs}" . ($isArray ? "[]" : "");
+    }
+
+}
+
+?>
